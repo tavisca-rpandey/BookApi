879fac72cafa3cfaa108b48f5cca910f95bba6a0
tavisca-rpandey <51921974+tavisca-rpandey@users.noreply.github.com>
2019-09-05 21:53:14 +0530

    Add files via upload

---
+++ BookApi/Controllers/BookContoller.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookApi.Model;
+using BookApi.Service;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookApi.Controllers
+{
+    [Route("api/book")]
+    public class BookContoller : Controller
+    {
+        BookService Service = new BookService();
+        
+        // GET: api/book
+        [HttpGet]
+        public string Get()
+        {
+            return Service.GetBooks();
+        }
+
+        // GET api/book/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return Service.GetBookName(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody] Book book)
+        {
+            return Service.AddBook(book);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody]Book newBook)
+        {
+            return Service.ModifyBookList(id, newBook);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return Service.DeleteBook(id);
+        }
+    }
+}
---
+++ BookApi/Data/BookData.cs
@@ -0,0 +1,36 @@
+﻿using BookApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Data
+{
+    public class BookData
+    {
+
+        private List<Book> _bookList = new List<Book>();
+        public BookData()
+        {
+            _bookList.Add(new Book { id = 1, Name = "Angels and Demons", Author="Xing Ho", Price=4500.65 });
+            _bookList.Add(new Book { id = 2, Name = "The matrix", Author = "Xing Ho", Price = 4500.65 });
+            _bookList.Add(new Book { id = 3, Name = "FairyTail", Author = "Xing Ho", Price = 4500.65 });
+           
+        }
+        public List<Book> GetBookList()
+        {
+            return _bookList;
+        }
+
+        public void Add(Book book)
+        {
+            _bookList.Add(book);
+            
+        }
+
+        public void RemoveAt(int i)
+        {
+            _bookList.RemoveAt(i);
+        }
+    }
+}
