a218d8442fdb2095d04122e5788b68a5eb0a30cc
tavisca-rpandey <51921974+tavisca-rpandey@users.noreply.github.com>
2019-09-06 11:33:48 +0530

    Added The tests Cases

--- BookApiXTests/UnitTest1.cs
+++ BookApiXTests/UnitTest1.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using BookApi.Data;
+using System.Linq;
 
 namespace BookApiXTests
 {
@@ -25,7 +27,41 @@ namespace BookApiXTests
             Assert.False(new Validation().isValidBook(book));
         }
 
+        [Fact]
+        public void AreAllBooksReturned()
+        {
+            BookData book = new BookData();
+            BookService serv = new BookService();
 
+            Assert.Equal(3,serv.GetBooks().Count());
+        }
+        [Fact]
+        public void IsTheCorrectBookReturned()
+        {
+            BookData book = new BookData();
+            BookService serv = new BookService();
 
+            Assert.Equal(book._bookList[0].Name, serv.GetBookName(1));
+        }
+
+        [Fact]
+        public void IsNewBookAdded()
+        {
+            Book book = new Book { id = 1, Name = "AngelsandDemons", Author = "XingHo", Price = 4500.65 };
+            BookService serv = new BookService();
+            serv.AddBook(book);
+
+            Assert.Equal(4, serv.GetBooks().Count());
+
+        }
+
+        [Fact]
+        public void IsBookDeleted()
+        {
+            BookService serv = new BookService();
+            serv.DeleteBook(1);
+
+            Assert.Equal(2, serv.GetBooks().Count());
+        }
     }
 }
